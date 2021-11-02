0dccc1e42c00a3af049498712896b84cd9dec741
GabrielaMRs <93285290+GabrielaMRs@users.noreply.github.com>
2021-11-02 10:12:57 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,13 @@
 window.onload = function inicio() {
     let nome = window.prompt('Qual é o seu nome?')
     let res = window.document.getElementById('resultado')
-    // Também funciona com o comando abaixo
-    res.innerHTML = `<p>Olá, <strong>${nome}</strong> seja bem-vindo(a) ao maior blog para cryptoinvestidores. &#x1F596;`
+    res.innerHTML = `<p>Olá, <strong>${nome}</strong> seja bem-vindo(a) ao maior blog para cryptoinvestidores.`
+}
+
+let contador = 4 
+let res = document.querySelector('section#result')
+
+function contar() {
+    contador ++ 
+    res.innerHTML = `${contador}`
 }
